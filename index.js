3e2ae5f8139aaf6108b4b3fa4a2c2802e5560f23
IssacPark7938 <134672182+IssacPark7938@users.noreply.github.com>
2023-10-23 17:47:13 +1300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ document.querySelector("button").addEventListener("click", function() {
     var name1 = document.getElementById("name1").value;
     var name2 = document.getElementById("name2").value;
 
-        var lovePercentage = Math.random() * 100;
-        lovePercentage = Math.floor(lovePercentage) + 1;
+    if (name1 === "" || name2 === "") {
+        alert("Please insert name/names");
+        return;
+    }
 
-        document.querySelector("h1").innerHTML= "The Loverate between " + name1 + " and " + name2 + " is " + lovePercentage + "%!";
-        console.log("The Loverate between " + name1 + " and " + name2 + " is " + lovePercentage + "%!");
+    var lovePercentage = Math.random() * 100;
+    lovePercentage = Math.floor(lovePercentage) + 1;
 
-        document.getElementById("name1").value = "";
-        document.getElementById("name2").value = "";
+    document.querySelector("h1").innerHTML= "The Loverate between " + name1 + " and " + name2 + " is " + lovePercentage + "%!";
+    console.log("The Loverate between " + name1 + " and " + name2 + " is " + lovePercentage + "%!");
+
+    document.getElementById("name1").value = "";
+    document.getElementById("name2").value = "";
 });
