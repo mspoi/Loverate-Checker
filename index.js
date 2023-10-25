7af0e3590dcc91a481580440bcdf80cb769de3e2
IssacPark7938 <134672182+IssacPark7938@users.noreply.github.com>
2023-10-26 09:24:09 +1300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ document.querySelector("button").addEventListener("click", function() {
     var name1 = document.getElementById("name1").value;
     var name2 = document.getElementById("name2").value;
 
-    var firstChar1 = name1.slice(0,1);
-    var firstChar2 = name2.slice(0,1);
-    var upperCaseFirstChar1 = firstChar1.toUpperCase();
-    var upperCaseFirstChar2 = firstChar2.toUpperCase();
-    var restOfName1 = name.slice(1,name1.length);
-    var restOfName2 = name.slice(1,name2.length);
-    restOfName1 = restOfName1.toLowerCase();
-    restOfName2 = restOfName2.toLowerCase();
-    var name11 = upperCaseFirstChar1 + restOfName1;
-    var name22 = upperCaseFirstChar2 + restOfName2;
-
     if (name1 === "" || name2 === "") {
         alert("Please insert name/names");
         return;
