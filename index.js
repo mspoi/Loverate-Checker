b56f0eb0d27750cceda3d05b2857f98ce6b2f79b
IssacPark7938 <134672182+IssacPark7938@users.noreply.github.com>
2023-10-26 09:21:51 +1300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ document.querySelector("button").addEventListener("click", function() {
     var firstChar2 = name2.slice(0,1);
     var upperCaseFirstChar1 = firstChar1.toUpperCase();
     var upperCaseFirstChar2 = firstChar2.toUpperCase();
-    var re
+    var restOfName1 = name.slice(1,name1.length);
+    var restOfName2 = name.slice(1,name2.length);
+    restOfName1 = restOfName1.toLowerCase();
+    restOfName2 = restOfName2.toLowerCase();
+    var name11 = upperCaseFirstChar1 + restOfName1;
+    var name22 = upperCaseFirstChar2 + restOfName2;
 
     if (name1 === "" || name2 === "") {
         alert("Please insert name/names");
@@ -16,8 +21,8 @@ document.querySelector("button").addEventListener("click", function() {
     var lovePercentage = Math.random() * 100;
     lovePercentage = Math.floor(lovePercentage) + 1;
 
-    document.querySelector("h1").innerHTML= "The Loverate between " + name1 + " and " + name2 + " is " + lovePercentage + "%!";
-    console.log("The Loverate between " + name1 + " and " + name2 + " is " + lovePercentage + "%!");
+    document.querySelector("h1").innerHTML= "The Loverate between " + name11 + " and " + name22 + " is " + lovePercentage + "%!";
+    console.log("The Loverate between " + name11 + " and " + name22 + " is " + lovePercentage + "%!");
 
     document.getElementById("name1").value = "";
     document.getElementById("name2").value = "";
